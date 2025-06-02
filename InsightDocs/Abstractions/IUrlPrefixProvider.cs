bd6d48d18e37ac0fe79400f184a1ba70926e6998
Luke Stratman <lstratman@gmail.com>
2025-06-02 15:55:01 -0400

    Added IUrlPrefixProvider service.

---
+++ InsightDocs/Abstractions/IUrlPrefixProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace InsightDocs.Abstractions;
+
+public interface IUrlPrefixProvider
+{
+    string? UrlPrefix
+    {
+        get;
+        set;
+    }
+}
---
+++ InsightDocs/Services/UrlPrefixProvider.cs
@@ -0,0 +1,17 @@
+﻿using InsightDocs.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsightDocs.Services;
+
+public class UrlPrefixProvider : IUrlPrefixProvider
+{
+    public string? UrlPrefix
+    {
+        get;
+        set;
+    }
+}
