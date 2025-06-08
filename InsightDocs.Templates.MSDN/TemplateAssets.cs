d6f8494fbf0c0b093a30dd52aefb96cc1e4f8db6
Luke Stratman <lstratman@gmail.com>
2025-06-08 18:20:10 -0400

    Moved template asset resources into a new folder structure.

--- InsightDocs.Templates.MSDN/TemplateAssets.cs
+++ InsightDocs.Templates.MSDN/TemplateAssets.cs
@@ -7,25 +7,25 @@ public class TemplateAssets
     public static ITemplateAsset MsdnCss 
     { 
         get; 
-    } = new EmbeddedResourceTemplateAsset(typeof(TemplateAssets).Assembly, "Assets", "msdn.css");
+    } = new EmbeddedResourceTemplateAsset(typeof(TemplateAssets).Assembly, "Assets", "css/msdn.css");
 
     public static ITemplateAsset DoconsFont
     {
         get;
-    } = new EmbeddedResourceTemplateAsset(typeof(TemplateAssets).Assembly, "Assets", "docons.woff2"); 
+    } = new EmbeddedResourceTemplateAsset(typeof(TemplateAssets).Assembly, "Assets", "css/docons.woff2"); 
     
     public static ITemplateAsset SegoeUIFont
     {
         get;
-    } = new EmbeddedResourceTemplateAsset(typeof(TemplateAssets).Assembly, "Assets", "segoe-ui.woff2");
+    } = new EmbeddedResourceTemplateAsset(typeof(TemplateAssets).Assembly, "Assets", "css/segoe-ui.woff2");
 
     public static ITemplateAsset SegoeUIRomanVfFont
     {
         get;
-    } = new EmbeddedResourceTemplateAsset(typeof(TemplateAssets).Assembly, "Assets", "segoe-ui-roman-vf.woff2");
+    } = new EmbeddedResourceTemplateAsset(typeof(TemplateAssets).Assembly, "Assets", "css/segoe-ui-roman-vf.woff2");
 
     public static ITemplateAsset HighlightJsScript
     {
         get;
-    } = new EmbeddedResourceTemplateAsset(typeof(TemplateAssets).Assembly, "Assets", "highlightjs/highlight.min.js");
+    } = new EmbeddedResourceTemplateAsset(typeof(TemplateAssets).Assembly, "Assets", "js/highlight.min.js");
 }
