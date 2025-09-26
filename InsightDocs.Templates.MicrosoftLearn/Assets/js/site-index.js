991b110f1f319f1e5d7cdc602d255688cc161266
Luke Stratman <lstratman@gmail.com>
2025-09-26 11:14:58 -0400

    Collapsing TOC items when clicking on the tree item text should not occur when the tree item is a link.

--- InsightDocs.Templates.MicrosoftLearn/Assets/js/site-index.js
+++ InsightDocs.Templates.MicrosoftLearn/Assets/js/site-index.js
@@ -122,8 +122,12 @@
         }
 
         else if (target.classList.contains('is-expanded')) {
-            target.classList.remove('is-expanded');
-            target.classList.add('is-collapsed');
+            let itemIndex = parseInt(target.getAttribute('data-toc-item-index'));
+
+            if (!tocData.Items[itemIndex].u) {
+                target.classList.remove('is-expanded');
+                target.classList.add('is-collapsed');
+            }
         }
     });
 
