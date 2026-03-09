148efd8dde775be4e6caf49fdb25c8c1906017e9
Luke Stratman <lstratman@gmail.com>
2026-03-09 10:50:31 -0400

    Added an extension point to the TypeScriptInterface Razor template to allow child classes to inject their own HTML.

---
+++ InsightDocs.Templates.MicrosoftLearn/TypeScript/TypeScriptInterfaceBase.cs
@@ -0,0 +1,15 @@
+﻿using InsightDocs.TypeScript;
+using Microsoft.AspNetCore.Components;
+
+namespace InsightDocs.Templates.MicrosoftLearn.TypeScript
+{
+    public class TypeScriptInterfaceBase : RazorTypeScriptTemplate<InsightDocs.TypeScript.Model.TypeScriptInterface>
+    {
+        public virtual RenderFragment AddAdditionalInterfaceProperties(InsightDocs.TypeScript.Model.TypeScriptInterface typeScriptInterface)
+        {
+            return (builder) =>
+            {
+            };
+        }
+    }
+}
