c5d566e2ab54988044daa2c819bfc2d952e4f3ab
Luke Stratman <lstratman@gmail.com>
2026-03-10 11:48:34 -0400

    Added an extension point to the TypeScriptMethod Razor template to allow child classes to inject their own HTML for method signatures.

---
+++ InsightDocs.Templates.MicrosoftLearn/TypeScript/TypeScriptMethodBase.cs
@@ -0,0 +1,15 @@
+﻿using InsightDocs.TypeScript;
+using Microsoft.AspNetCore.Components;
+
+namespace InsightDocs.Templates.MicrosoftLearn.TypeScript
+{
+    public class TypeScriptMethodBase : RazorTypeScriptTemplate<InsightDocs.TypeScript.Model.TypeScriptMethod>
+    {
+        public virtual RenderFragment AddAdditionalMethodSignatureProperties(InsightDocs.TypeScript.Model.TypeScriptMethodSignature typeScriptMethodSignature)
+        {
+            return (builder) =>
+            {
+            };
+        }
+    }
+}
